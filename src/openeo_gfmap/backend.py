b0cac9e9814fefc5ae550486a76e2a71c3535f21
Stefaan Lippens <stefaan.lippens@vito.be>
2024-01-24 10:43:51 +0100

    Issue #9 initial support for multiple client cred conf through env vars

--- src/openeo_gfmap/backend.py
+++ src/openeo_gfmap/backend.py
@@ -2,11 +2,15 @@
 
 Defines on which backend the pipeline is being currently used.
 """
+import logging
+import os
 from dataclasses import dataclass
 from enum import Enum
-from typing import Callable, Dict
+from typing import Callable, Dict, Optional
 
 import openeo
+
+_log = logging.getLogger(__name__)
 
 
 class Backend(Enum):
@@ -29,32 +33,57 @@ class BackendContext:
     backend: Backend
 
 
+def _create_connection(
+    url: str, *, env_var_suffix: str, connect_kwargs: Optional[dict] = None
+):
+    """
+    Generic helper to create an openEO connection
+    with support for multiple client credential configurations from environment variables
+    """
+    connection = openeo.connect("openeo.vito.be", **(connect_kwargs or {}))
+
+    if (
+        os.environ.get("OPENEO_AUTH_METHOD") == "client_credentials"
+        and f"OPENEO_AUTH_CLIENT_ID_{env_var_suffix}" in os.environ
+    ):
+        client_id = os.environ[f"OPENEO_AUTH_CLIENT_ID_{env_var_suffix}"]
+        client_secret = os.environ[f"OPENEO_AUTH_CLIENT_SECRET_{env_var_suffix}"]
+        provider_id = os.environ.get(f"OPENEO_AUTH_PROVIDER_ID_{env_var_suffix}")
+        _log.info(
+            f"Doing client credentials from env var with {env_var_suffix=} {provider_id} {client_id=} {len(client_secret)=} "
+        )
+
+        connection.authenticate_oidc_client_credentials(
+            client_id=client_id, client_secret=client_secret, provider_id=provider_id
+        )
+    else:
+        # Standard authenticate_oidc procedure: refresh token, device code or default env var handling
+        # See https://open-eo.github.io/openeo-python-client/auth.html#oidc-authentication-dynamic-method-selection
+
+        # Use a shorter max poll time by default to alleviate the default impression that the test seem to hang
+        # because of the OIDC device code poll loop.
+        max_poll_time = int(
+            os.environ.get("OPENEO_OIDC_DEVICE_CODE_MAX_POLL_TIME") or 30
+        )
+        connection.authenticate_oidc(max_poll_time=max_poll_time)
+    return connection
+
+
 def vito_connection() -> openeo.Connection:
     """Performs a connection to the VITO backend using the oidc authentication."""
-    # Note: this generic `authenticate_oidc()` call allows both:
-    # - device code/refresh token based authentication for manual test
-    #   suiteruns by a developer
-    # - client credentials auth through env vars for automated/Jenkins CI runs
-    #
-    # See https://open-eo.github.io/openeo-python-client/auth.html#oidc-authentication-dynamic-method-selection  # NOQA
-    # and Jenkinsfile, where Jenkins fetches the env vars from VITO TAP Vault.
-    connection = openeo.connect("openeo.vito.be")
-    connection.authenticate_oidc()
-    return connection
+    return _create_connection("openeo.vito.be", env_var_suffix="VITO")
 
 
 def cdse_connection() -> openeo.Connection:
     """Performs a connection to the CDSE backend using oidc authentication."""
-    connection = openeo.connect("https://openeo.dataspace.copernicus.eu/openeo/1.2")
-    connection.authenticate_oidc()
-    return connection
+    return _create_connection("openeo.dataspace.copernicus.eu", env_var_suffix="CDSE")
 
 
 def eodc_connection() -> openeo.Connection:
     """Perfroms a connection to the EODC backend using the oidc authentication."""
-    connection = openeo.connect("https://openeo.eodc.eu/openeo/1.1.0")
-    connection.authenticate_oidc()
-    return connection
+    return _create_connection(
+        "https://openeo.eodc.eu/openeo/1.1.0", env_var_suffix="CDSE"
+    )
 
 
 BACKEND_CONNECTIONS: Dict[Backend, Callable] = {
