eaf3d8a12612d5e3125d08425f579cd31b21fab8
Darius Couchard <darius.couchard@vito.be>
2024-04-15 09:45:13 +0200

    Added timeout to request fething on resources

--- tests/test_openeo_gfmap/utils.py
+++ tests/test_openeo_gfmap/utils.py
@@ -9,7 +9,7 @@ import xarray as xr
 def load_dataset_url(url: str) -> NamedTemporaryFile:
     """Download a NetCDF file from the internet and return a Xarray Dataset."""
     with NamedTemporaryFile(suffix=".nc", delete=True) as tmpfile:
-        response = requests.get(url)
+        response = requests.get(url, timeout=60)
         response.raise_for_status()
         tmpfile.write(response.content)
 
@@ -21,7 +21,7 @@ def load_dataset_url(url: str) -> NamedTemporaryFile:
 def load_dataarray_url(url: str) -> NamedTemporaryFile:
     """Download a NetCDF file from the internet and return a Xarray Dataset."""
     with NamedTemporaryFile(suffix=".nc", delete=True) as tmpfile:
-        response = requests.get(url)
+        response = requests.get(url, timeout=60)
         response.raise_for_status()
         tmpfile.write(response.content)
 
