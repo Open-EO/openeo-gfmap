f9554cf881e3f02e3dd333ba17319045394aa01b
Darius Couchard <darius.couchard@vito.be>
2024-05-22 11:18:12 +0200

    Added log setup by default for gfmap manager

--- src/openeo_gfmap/manager/__init__.py
+++ src/openeo_gfmap/manager/__init__.py
@@ -4,3 +4,27 @@ managing the subjobs.
 import logging
 
 _log = logging.getLogger(__name__)
+
+_log.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+_log.addHandler(stream_handler)
+
+formatter = logging.Formatter("%(asctime)s|%(name)s|%(levelname)s:  %(message)s")
+stream_handler.setFormatter(formatter)
+
+
+# Exclude the other loggers from other libraries
+class ManagerLoggerFilter(logging.Filter):
+    """Filter to only accept the OpenEO-GFMAP manager logs."""
+
+    def filter(self, record):
+        return record.name in [_log.name]
+
+
+stream_handler.addFilter(ManagerLoggerFilter())
+
+
+def set_log_level(level):
+    """Set the log level of the OpenEO-GFMAP manager logger."""
+    _log.setLevel(level)
