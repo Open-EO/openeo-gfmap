0544864a32ec89b58cd6230d71140ac5690977dc
Darius Couchard <darius.couchard@vito.be>
2024-05-22 11:22:40 +0200

    Fix imports order

--- src/openeo_gfmap/manager/job_manager.py
+++ src/openeo_gfmap/manager/job_manager.py
@@ -1,7 +1,7 @@
 import json
-from functools import partial
 from concurrent.futures import ThreadPoolExecutor
 from enum import Enum
+from functools import partial
 from pathlib import Path
 from typing import Callable, Optional, Union
 
