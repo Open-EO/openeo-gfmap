7823dd33630db4a847c3d4863da8a57d08b6a0f7
Kristof Van Tricht <kristof.vantricht@vito.be>
2024-09-09 17:29:11 +0200

    Add logging to utils.py

--- src/openeo_gfmap/utils/__init__.py
+++ src/openeo_gfmap/utils/__init__.py
@@ -1,5 +1,7 @@
 """This sub-module contains utilitary function and tools for OpenEO-GFMap"""
 
+import logging
+
 from openeo_gfmap.utils.build_df import load_json
 from openeo_gfmap.utils.intervals import quintad_intervals
 from openeo_gfmap.utils.netcdf import update_nc_attributes
@@ -11,6 +13,18 @@ from openeo_gfmap.utils.tile_processing import (
     select_optical_bands,
     select_sar_bands,
 )
+
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+
+_log.addHandler(ch)
+
 
 __all__ = [
     "load_json",
