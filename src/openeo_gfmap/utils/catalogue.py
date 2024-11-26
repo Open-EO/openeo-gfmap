634bb8f8f433aa4a522dadda7a06ede401bb6115
Vincent Verelst <vincent.verelst@vverelst.vitovm.rscluster.vito.be>
2024-11-26 10:01:31 +0100

    fix isort #180

--- src/openeo_gfmap/utils/catalogue.py
+++ src/openeo_gfmap/utils/catalogue.py
@@ -8,7 +8,7 @@ import requests
 from pyproj.crs import CRS
 from rasterio.warp import transform_bounds
 from requests import adapters
-from shapely.geometry import box, Point, shape
+from shapely.geometry import Point, box, shape
 from shapely.ops import unary_union
 
 from openeo_gfmap import (
@@ -206,7 +206,7 @@ def s1_area_per_orbitstate_vvvh(
         ]
         if len(shapely_geometries) == 1 and isinstance(shapely_geometries[0], Point):
             point = shapely_geometries[0]
-            buffer_size = 0.0001 
+            buffer_size = 0.0001
             buffered_geometry = point.buffer(buffer_size)
             bounds = buffered_geometry.bounds
         else:
