a08149a6494042d2938638d16b4d7c1fab694f31
Kristof Van Tricht <kristof.vantricht@vito.be>
2024-12-20 08:05:05 +0100

    Fix sar rescaling bug for orfeo

--- src/openeo_gfmap/preprocessing/sar.py
+++ src/openeo_gfmap/preprocessing/sar.py
@@ -27,38 +27,17 @@ def compress_backscatter_uint16(
     openeo.DataCube
         The datacube with the backscatter values compressed to uint16.
     """
-    backend = backend_context.backend
 
-    # Additional check related to problematic values present in creodias collections.
-    # https://github.com/Open-EO/openeo-geopyspark-driver/issues/293
-    if backend in [Backend.CDSE, Backend.CDSE_STAGING, Backend.FED]:
-        cube = cube.apply_dimension(
-            dimension="bands",
-            process=lambda x: array_create(
-                [
-                    if_(
-                        is_nodata(x[0]),
-                        1,
-                        power(base=10, p=(10.0 * x[0].log(base=10) + 83.0) / 20.0),
-                    ),
-                    if_(
-                        is_nodata(x[1]),
-                        1,
-                        power(base=10, p=(10.0 * x[1].log(base=10) + 83.0) / 20.0),
-                    ),
-                ]
-            ),
-        )
-    else:
-        cube = cube.apply_dimension(
-            dimension="bands",
-            process=lambda x: array_create(
-                [
-                    power(base=10, p=(10.0 * x[0].log(base=10) + 83.0) / 20.0),
-                    power(base=10, p=(10.0 * x[1].log(base=10) + 83.0) / 20.0),
-                ]
-            ),
-        )
+    # Apply rescaling of power values in a logarithmic way
+    cube = cube.apply_dimension(
+        dimension="bands",
+        process=lambda x: array_create(
+            [
+                power(base=10, p=(10.0 * x[0].log(base=10) + 83.0) / 20.0),
+                power(base=10, p=(10.0 * x[1].log(base=10) + 83.0) / 20.0),
+            ]
+        ),
+    )
 
     # Change the data type to uint16 for optimization purposes
     return cube.linear_scale_range(1, 65534, 1, 65534)
